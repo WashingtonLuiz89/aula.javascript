825d34e11f75ad5b355b3854672a81912cb28e78
unknown <wl-cg@hotmail.com>
2020-08-27 00:36:51 -0300

    atulalizando

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
+var d = new Date();
+ alert(d.getMonth());
+ alert(d.getDay());
+ alert(d.getMinutes());
 
-var nome = "Washington Luiz";
-var idade = 29;
-var idade2 = 2;
-var frase= "japão eo melhor time do mundo";
+/*var count;
+for(count=0; count<=5;count++){
+    alert(count);
+}
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+    //count = count +1;
+};
+/*var nome = "Washington Luiz";
+var idade = prompt("qual sua idade");
+//var idade = 29;
+//var idade2 = 2;
+if (idade > 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+
+/*var frutas = [{nome: "maça", Cor: "Vermelha"},{nome:"uva, cor:"roxa}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+var lista = ["maçã", "perâ", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista);
+*/
+//var frase= "japão eo melhor time do mundo";
 //alert(nome+" tem "+ idade + "anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(idade+idade2);
+//console.log(nome);
+//console.log(idade+idade2);
 //console.log(frase.replace("japão", "brasil"));
 //alert(frase.replace("japão", "brasil"));
-console.log(frase .toLowerCase());
+//console.log(frase .toLowerCase());
