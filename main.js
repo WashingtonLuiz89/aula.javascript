7ee87ac1783062738b2e3b8c09c4b3adda1768e5
unknown <wl-cg@hotmail.com>
2020-08-26 22:49:43 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var nome = "Washington Luiz";
+var idade = 29;
+var idade2 = 2;
+var frase= "japão eo melhor time do mundo";
+//alert(nome+" tem "+ idade + "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+//console.log(frase.replace("japão", "brasil"));
+//alert(frase.replace("japão", "brasil"));
+console.log(frase .toLowerCase());
